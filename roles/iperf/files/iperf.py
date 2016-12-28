08446d1466c7147ee6d78be8b49603cf8f3660f1
haowang <wh.sjtu@gmail.com>
2016-12-28 15:33:05 -0500

    update

--- roles/iperf/files/iperf.py
+++ roles/iperf/files/iperf.py
@@ -2,7 +2,29 @@
 
 import os, sys
 import subprocess
+import yaml
+import os.path as path
+
+EXEC_DIR = path.dirname(path.abspath(__file__))
+
+## parse config file
+with open('./iperf.yml', 'r') as yml_fd:
+    cfg = yaml.load(yml_fd)
 
 
+hosts = []
+ASYNC = cfg['async']
+
+
+iperf_cmd = "iperf -c %s -p %d -t %d -P %d"
+
+for host in hosts:
+	if ASYNC:
+		subprocess.Popen(, shell=True)
+	else:
+		subprocess.call()
+
+
+if ASYNC:
 
 
